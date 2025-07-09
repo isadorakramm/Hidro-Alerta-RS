37d72b9410baa73d7d91c92a3888e631af245332
Isa Kramm <134699029+isadorakramm@users.noreply.github.com>
2025-07-09 11:01:53 -0300

    ajuste na interface

--- info.php
+++ info.php
@@ -3,75 +3,68 @@
     include_once './include/header.php';
 ?>
 <body class="info">
-    <main>
-        <div class="section_bloco">
-        <div class="txt_geral">
-            <h2>
-                Entendendo o fenômeno natural
-            </h2>
-            <p>Para uma análise mais aprofundada do fenômeno, é fundamental considerar diversos elementos que podem culminar em uma enchente. Como amplamente conhecido por aqueles que presenciaram o desastre natural ocorrido no Rio Grande do Sul durante o final de 2023 e inicio de 2024, o Ciclone Extratropical foi um dos principais causadores da tragédia. Contudo, outros fatores, sejam eles de origem humana ou ambiental, também desempenharam papéis significativos. </p>
-        </div>
-        <div class="container">
-            <button class="arrow-left control" aria-label="Previous image">◀</button>
-            <button class="arrow-right control" aria-label="Next Image">▶</button>
-            <div class="gallery-wrapper">
-                <div class="gallery">
-                    <a href="https://abrasco.org.br/enchentes-no-rio-grande-do-sul-desafios-para-a-saude-publica-em-meio-a-catastrofe/" target="_blank" class="item current-item">
-                        <img src="imagens/1.png" alt="Imagem da notícia">
-                        <div class="caption">Desafios para a saúde pública em meio à catástrofe</div>
-                    </a>
+    <main style="padding: 2rem; max-width: 80%; margin: auto; flex: 1;">
+        <section class="section_bloco">
+            <article class="textoInfo">
+                <h2>Entendendo o fenômeno natural</h2>
+                <p>Para uma análise mais aprofundada do fenômeno, é fundamental considerar diversos elementos que podem culminar em uma enchente. Como amplamente conhecido por aqueles que presenciaram o desastre natural ocorrido no Rio Grande do Sul durante o final de 2023 e inicio de 2024, o Ciclone Extratropical foi um dos principais causadores da tragédia. Contudo, outros fatores, sejam eles de origem humana ou ambiental, também desempenharam papéis significativos. </p>
+            </article>      
 
-                    <a href="https://www.brasildefato.com.br/2024/05/16/enchentes-ja-impactam-461-dos-497-municipios-do-rio-grande-do-sul/" target="_blank" class="item">
-                        <img src="imagens/3.png" alt="Imagem da notícia">
-                        <div class="caption">Enchentes já impactam 461 dos 497 municípios do RS</div>
-                    </a>
+            <div class="container">
+                <button class="arrow-left control" aria-label="Previous image">◀</button>
+                <button class="arrow-right control" aria-label="Next Image">▶</button>
+                <div class="gallery-wrapper">
+                    <div class="gallery">
+                        <a href="https://abrasco.org.br/enchentes-no-rio-grande-do-sul-desafios-para-a-saude-publica-em-meio-a-catastrofe/" target="_blank" class="item current-item">
+                            <img src="imagens/1.png" alt="Imagem da notícia">
+                            <div class="caption">Desafios para a saúde pública em meio à catástrofe</div>
+                        </a>
 
-                    <a href="https://www.correiodopovo.com.br/notícias/cidades/imagens-de-satélite-mostram-dimensões-das-enchentes-de-junho-de-2025-no-rs-1.1623141" target="_blank" class="item">
-                        <img src="imagens/2.png" alt="Imagem da notícia">
-                        <div class="caption">Imagens de satélite mostram enchentes de junho de 2025 no RS</div>
-                    </a>
+                        <a href="https://www.brasildefato.com.br/2024/05/16/enchentes-ja-impactam-461-dos-497-municipios-do-rio-grande-do-sul/" target="_blank" class="item">
+                            <img src="imagens/3.png" alt="Imagem da notícia">
+                            <div class="caption">Enchentes já impactam 461 dos 497 municípios do RS</div>
+                        </a>
+
+                        <a href="https://www.correiodopovo.com.br/notícias/cidades/imagens-de-satélite-mostram-dimensões-das-enchentes-de-junho-de-2025-no-rs-1.1623141" target="_blank" class="item">
+                            <img src="imagens/2.png" alt="Imagem da notícia">
+                            <div class="caption">Imagens de satélite mostram enchentes de junho de 2025 no RS</div>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-        </div>
-        <div class="txt_geral">
-            <h2>Possíveis causas</h2>
-            <p>A atividade humana no meio ambiente é um dos pontos cruciais a serem considerados ao realizar uma tentativa de compreender melhor o fenômeno natural. A gestão inadequada de resíduos sempre representou uma preocupação ambiental global. É inegável que esse é um fator que contribui de maneira substancial para o agravamento das enchentes. </p>
-            <p>Apesar das tentativas realizadas pelo governo brasileiro de reduzir o descarte de resíduos nas vias públicas, este problema persiste de forma significativa em todo o país. Embora o descarte inadequado não seja um grande fator determinante para os desastres naturais, ainda sim deve ser mencionado devido à sua capacidade de agravar a situação, aumentando a propensão de determinadas áreas a sofrerem com enchentes. </p>
-        </div>
-        <div class="txt_geral">
-            <p>A incineração de resíduos é ainda mais preocupante devido aos seus impactos na atmosfera, exacerbando significativamente o aquecimento global. Esta prática libera quantidades consideráveis de dióxido de carbono (CO2) e outros gases de efeito estufa na atmosfera, contribuindo diretamente para as mudanças climáticas.</p>
-            <p>Como resultado das mudanças climáticas, é relevante destacar a intensificação do fenômeno El Niño, diretamente relacionado às enchentes de 2023. O El Niño é caracterizado pelo aquecimento desproporcional das águas do oceano Pacífico e as consequências desse evento para a região sul do Brasil é uma forte intensificação no volume das chuvas nos estados que compõem a região. Normalmente, esse fenômeno ocorre em intervalos de tempos que variam de cinco a sete anos, entretanto, conforme as mudanças climáticas antropogênicas, existe uma tendência de aumento em sua frequência e intensidade.</p>
-        </div>
-        <div class="txt_geral">
-            <h2>Consequências</h2>
-            <p>Agora que examinamos algumas das causas gerais das enchentes, é fundamental explorar suas consequências. É por meio da análise dessas consequências que podemos compreender verdadeiramente a gravidade de uma tragédia e levantas hipóteses sobre possíveis soluções para evitar que ocorra novamente na mesma intensidade.</p>
-            <p>As enchentes podem levar aos locais afetados graves impactos ambientais para o ecossistema e, por consequência, à população ao seu redor. Entre umas das principais consequências para o meio ambiente estão a contaminação da água. Ao atingir áreas não planejadas, a água tem alta probabilidade de entrar em contato com substâncias perigosas, representando um risco significativo para tudo que ela venha a tocar em algum momento. Esse perigo aumenta ainda mais em regiões industriais ou agrícolas.</p>
-            <p>A incineração de resíduos é ainda mais preocupante devido aos seus impactos na atmosfera, exacerbando significativamente o aquecimento global. Esta prática libera quantidades consideráveis de dióxido de carbono (CO2) e outros gases de efeito estufa na atmosfera, contribuindo diretamente para as mudanças climáticas.</p>
-        </div>
-        <div class="txt_geral">
-            <p>Outro fenômeno que pode surgir à longo prazo é a estiagem. A estiagem é caracterizada quando há um atraso superior a quinze dias do início da temporada de chuvas e quando as médias de precipitação mensais se mantêm inferiores a 60% das médias mensais usuais. Em um estado como o Rio Grande do Sul, onde a agricultura exerce um papel importante para a economia, esse fenômeno é muito perigoso. A estiagem pode impactar diretamente na produtividade das colheitas e, por consequência, na economia do estado inteiro e até mesmo do resto do país.</p>
-            <p>Como resultado das mudanças climáticas, é relevante destacar a intensificação do fenômeno El Niño, diretamente relacionado às enchentes de 2023. O El Niño é caracterizado pelo aquecimento desproporcional das águas do oceano Pacífico e as consequências desse evento para a região sul do Brasil é uma forte intensificação no volume das chuvas nos estados que compõem a região. Normalmente, esse fenômeno ocorre em intervalos de tempos que variam de cinco a sete anos, entretanto, conforme as mudanças climáticas antropogênicas, existe uma tendência de aumento em sua frequência e intensidade.</p>
-        </div>
-        <div class="txt_geral">
-            <p>A questão da saúde pública pode abranger tanto danos físicos quanto psicológicos. Entre os danos físicos, destacam-se principalmente as doenças transmitidas pela água contaminada, como leptospirose e dengue, resultantes da acumulação de água em alguns locais afetados pelas enchentes onde a água não foi removida. Além disso, é evidente o número significativo de vítimas fatais e feridos em decorrência de deslizamentos causados pelas chuvas.</p>
-            <p>Entre os danos psicológicos podemos listar o estresse pós-traumático, a insônia, a depressão, entre diversos outros impactos mentais que a angústia de sobreviver a uma catástrofe ambiental pode gerar.</p>
-            <p>A prova mais clara de que um desastre natural pode afetar o psicológico de uma pessoa é a existência de um termo chamado “Abobado da Enchente”. O termo surgiu no Rio Grande do Sul em 1941 e era utilizado com pessoas que foram diretamente afetadas pelas águas e sofreram de grandes perdas. Os ditos “abobados da enchente” eram pessoas que sofreram por tamanho trauma que as tornou totalmente apáticas e sem esperanças com a vida.</p>
-        </div>
-        <div class="txt_geral">
-            <p>Os danos à infraestrutura dos locais afetados são inúmeros, principalmente quando temos em mente que durante as enchentes do final de 2023, as águas chegaram a cobrir 60% de alguns municípios. Como resultado, a altura e a intensidade das águas causaram devastação completa em várias estruturas das cidades impactadas, incluindo desmoronamentos de prédios, casas, pontes, estradas, e viadutos, entre outros. Os prejuízos estruturais são predominantemente tangíveis, porém têm impactos diretos significativos no dia a dia dos residentes e na economia local.</p>
-        </div>
-        <div class="txt_geral">
-            <h2>Possíveis soluções</h2>
-            <p>Agora que passamos pelas causas e consequências das enchentes, podemos discutir algumas soluções hipotéticas.</p>
-            <p>Antes disso, é interessante apresentar como funciona o modelo UNDRO (United Nations Disaster Relief Organization), que visa avaliar a situação de cada local em busca de gerenciar os desastres naturais de uma forma organizada em cinco etapas: Identificação de riscos; Análise de riscos; Medidas de prevenção; Planejamento para emergências; Informações públicas e treinamento. O modelo UNDRO oferece uma abordagem estruturada para a preparação e gestão de crises ambientais, permitindo lidar com as situações de forma mais tranquila.</p>
-            <p>O modelo foi adaptado pela Defesa Civil Nacional (Brasília, 2007) e se compõe da seguinte forma:<br>1. Prevenção;<br>2. Preparação para Emergências e Desastres;<br>3. Resposta;<br>4. Resolução.</p>
-        </div>
-        
+        </section>
+        <section class="section_bloco">
+            <article>
+                <h2>Possíveis causas</h2>
+                <div class="bolhas">
+                    <p>A atividade humana no meio ambiente é um dos pontos cruciais a serem considerados ao realizar uma tentativa de compreender melhor o fenômeno natural. A gestão inadequada de resíduos sempre representou uma preocupação ambiental global. É inegável que esse é um fator que contribui de maneira substancial para o agravamento das enchentes. </p>
+                    <p>Apesar das tentativas realizadas pelo governo brasileiro de reduzir o descarte de resíduos nas vias públicas, este problema persiste de forma significativa em todo o país. Embora o descarte inadequado não seja um grande fator determinante para os desastres naturais, ainda sim deve ser mencionado devido à sua capacidade de agravar a situação, aumentando a propensão de determinadas áreas a sofrerem com enchentes. </p>
+                    <p>A incineração de resíduos é ainda mais preocupante devido aos seus impactos na atmosfera, exacerbando significativamente o aquecimento global. Esta prática libera quantidades consideráveis de dióxido de carbono (CO2) e outros gases de efeito estufa na atmosfera, contribuindo diretamente para as mudanças climáticas.</p>
+                    <p>Como resultado das mudanças climáticas, é relevante destacar a intensificação do fenômeno El Niño, diretamente relacionado às enchentes de 2023. O El Niño é caracterizado pelo aquecimento desproporcional das águas do oceano Pacífico e as consequências desse evento para a região sul do Brasil é uma forte intensificação no volume das chuvas nos estados que compõem a região. Normalmente, esse fenômeno ocorre em intervalos de tempos que variam de cinco a sete anos, entretanto, conforme as mudanças climáticas antropogênicas, existe uma tendência de aumento em sua frequência e intensidade.</p>
+                </div>
+                <h2>Consequências</h2>
+                <div class="bolhas">
+                    <p>Agora que examinamos algumas das causas gerais das enchentes, é fundamental explorar suas consequências. É por meio da análise dessas consequências que podemos compreender verdadeiramente a gravidade de uma tragédia e levantar hipóteses sobre possíveis soluções para evitar que ocorra novamente na mesma intensidade.</p>
+                    <p>As enchentes podem levar aos locais afetados graves impactos ambientais para o ecossistema e, por consequência, à população ao seu redor. Entre umas das principais consequências para o meio ambiente estão a contaminação da água. Ao atingir áreas não planejadas, a água tem alta probabilidade de entrar em contato com substâncias perigosas, representando um risco significativo para tudo que ela venha a tocar em algum momento. Esse perigo aumenta ainda mais em regiões industriais ou agrícolas.</p>
+                    <p>A incineração de resíduos é ainda mais preocupante devido aos seus impactos na atmosfera, exacerbando significativamente o aquecimento global. Esta prática libera quantidades consideráveis de dióxido de carbono (CO2) e outros gases de efeito estufa na atmosfera, contribuindo diretamente para as mudanças climáticas.</p>
+                    <p>Outro fenômeno que pode surgir à longo prazo é a estiagem. A estiagem é caracterizada quando há um atraso superior a quinze dias do início da temporada de chuvas e quando as médias de precipitação mensais se mantêm inferiores a 60% das médias mensais usuais. Em um estado como o Rio Grande do Sul, onde a agricultura exerce um papel importante para a economia, esse fenômeno é muito perigoso. A estiagem pode impactar diretamente na produtividade das colheitas e, por consequência, na economia do estado inteiro e até mesmo do resto do país.</p>
+                    <p>Como resultado das mudanças climáticas, é relevante destacar a intensificação do fenômeno El Niño, diretamente relacionado às enchentes de 2023. O El Niño é caracterizado pelo aquecimento desproporcional das águas do oceano Pacífico e as consequências desse evento para a região sul do Brasil é uma forte intensificação no volume das chuvas nos estados que compõem a região. Normalmente, esse fenômeno ocorre em intervalos de tempos que variam de cinco a sete anos, entretanto, conforme as mudanças climáticas antropogênicas, existe uma tendência de aumento em sua frequência e intensidade.</p>
+                    <p>A questão da saúde pública pode abranger tanto danos físicos quanto psicológicos. Entre os danos físicos, destacam-se principalmente as doenças transmitidas pela água contaminada, como leptospirose e dengue, resultantes da acumulação de água em alguns locais afetados pelas enchentes onde a água não foi removida. Além disso, é evidente o número significativo de vítimas fatais e feridos em decorrência de deslizamentos causados pelas chuvas.</p>
+                    <p>Entre os danos psicológicos podemos listar o estresse pós-traumático, a insônia, a depressão, entre diversos outros impactos mentais que a angústia de sobreviver a uma catástrofe ambiental pode gerar.</p>
+                    <p>A prova mais clara de que um desastre natural pode afetar o psicológico de uma pessoa é a existência de um termo chamado “Abobado da Enchente”. O termo surgiu no Rio Grande do Sul em 1941 e era utilizado com pessoas que foram diretamente afetadas pelas águas e sofreram de grandes perdas. Os ditos “abobados da enchente” eram pessoas que sofreram por tamanho trauma que as tornou totalmente apáticas e sem esperanças com a vida.</p>
+                    <p>Os danos à infraestrutura dos locais afetados são inúmeros, principalmente quando temos em mente que durante as enchentes do final de 2023, as águas chegaram a cobrir 60% de alguns municípios. Como resultado, a altura e a intensidade das águas causaram devastação completa em várias estruturas das cidades impactadas, incluindo desmoronamentos de prédios, casas, pontes, estradas, e viadutos, entre outros. Os prejuízos estruturais são predominantemente tangíveis, porém têm impactos diretos significativos no dia a dia dos residentes e na economia local.</p>
+                </div>
+                <h2>Possíveis soluções</h2>
+                <div class="bolhas">
+                    <p>Agora que passamos pelas causas e consequências das enchentes, podemos discutir algumas soluções hipotéticas.</p>
+                    <p>Antes disso, é interessante apresentar como funciona o modelo UNDRO (United Nations Disaster Relief Organization), que visa avaliar a situação de cada local em busca de gerenciar os desastres naturais de uma forma organizada em cinco etapas: Identificação de riscos; Análise de riscos; Medidas de prevenção; Planejamento para emergências; Informações públicas e treinamento. O modelo UNDRO oferece uma abordagem estruturada para a preparação e gestão de crises ambientais, permitindo lidar com as situações de forma mais tranquila.</p>
+                    <p>O modelo foi adaptado pela Defesa Civil Nacional (Brasília, 2007) e se compõe da seguinte forma:<br>1. Prevenção;<br>2. Preparação para Emergências e Desastres;<br>3. Resposta;<br>4. Resolução.</p>
+                </div>
+            </article>
+        </section>
     </main>
+    <?php include_once './include/footer.php'; ?>  
     <script src="script.js"></script>
-<?php
-    include_once './include/footer.php';
-?>
 </body>
 </html>
+ 
